c41c04a6af2e8971ceef40a6468153c6bf5daf1b
Victor Pikula <victor@pikula.com>
2014-10-31 23:10:44 +0100

    Created function for droplet creation and added comments

--- create.py
+++ create.py
@@ -3,62 +3,39 @@ import digitalocean
 import requests
 import time
 
+# Function that creates the droplets
+def createdroplet( number ):
+	droplet = digitalocean.Droplet(token=DO_TOKEN,
+                               name='coreos-{0}'.format(number),
+                               region='ams3',
+                               image='coreos-stable',
+                               size='512mb',
+                               private_networking=True,
+							   ssh_keys=[ssh_key.id],
+							   user_data=config_file)
+	droplet.create()
+	time.sleep(5)
+	droplet = manager.get_droplet(droplet.id)
+	#print "droplet #%s created with public ip address: %t" % (number, droplet.ip_address)
+	print "droplet #{0} created with public ip address: {1}".format(number, droplet.ip_address)
+	return
+
+# Load environment variables for DO token and SSH Key fingerprint
 DO_TOKEN = os.environ.get("DO_TOKEN", None)
 DO_FINGERPRINT = os.environ.get("DO_FINGERPRINT", None)
 
+# DO API init
 manager = digitalocean.Manager(token=DO_TOKEN)
-
 ssh_key = manager.get_ssh_key(DO_FINGERPRINT)
 
+# Get new etcd discovery uri
 discoveryUri = requests.get("https://discovery.etcd.io/new").content
 
+# Load template cloud-config file with discovery uri inserted
 with file('cloud-config') as f:
 	config_file = f.read().format(discoveryUri)
 
-droplet = digitalocean.Droplet(token=DO_TOKEN,
-                               name='coreos-1',
-                               region='ams3',
-                               image='coreos-stable',
-                               size='512mb',
-                               private_networking=True,
-							   ssh_keys=[ssh_key.id],
-							   user_data=config_file)
-droplet.create()
-
-time.sleep(5)
-
-droplet = manager.get_droplet(droplet.id)
-
-print "droplet #1 created with public ip address: %s" % droplet.ip_address
-
-droplet = digitalocean.Droplet(token=DO_TOKEN,
-                               name='coreos-2',
-                               region='ams3',
-                               image='coreos-stable',
-                               size='512mb',
-                               private_networking=True,
-							   ssh_keys=[ssh_key.id],
-							   user_data=config_file)
-droplet.create()
-
-time.sleep(5)
-
-droplet = manager.get_droplet(droplet.id)
-
-print "droplet #2 created with public ip address: %s" % droplet.ip_address
-
-droplet = digitalocean.Droplet(token=DO_TOKEN,
-                               name='coreos-3',
-                               region='ams3',
-                               image='coreos-stable',
-                               size='512mb',
-                               private_networking=True,
-							   ssh_keys=[ssh_key.id],
-							   user_data=config_file)
-droplet.create()
-
-time.sleep(5)
-
-droplet = manager.get_droplet(droplet.id)
-
-print "droplet #3 created with public ip address: %s" % droplet.ip_address
+# Create droplets
+createdroplet(1)
+createdroplet(2)
+createdroplet(3)
